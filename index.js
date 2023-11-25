fd9d569f4c55c8ef592b40233d2a1cba2c31b359
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-25 20:31:47 +0600

    create user collection

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ const client = new MongoClient(process.env.DB_URI, {
 })
 async function run() {
   try {
+
+    const usersCollection = client.db('skillify').collection('users')
+
+
+
     // auth related api
     app.post('/jwt', async (req, res) => {
       const user = req.body
